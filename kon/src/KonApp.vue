c6af74f5fb5553864a92bcdb40e64dd8989184f1
Savyan <mrsavyan555@gmail.com>
2024-04-13 12:53:46 +0300

    initial commit

---
+++ kon/src/KonApp.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="circle">KON</div>
+</template>
+
+<style scoped>
+.circle {
+    border: 10px dashed red;
+    height: 100px;
+    width: 100px;
+    border-radius: 50%;
+
+    display: flex; /* or inline-flex */
+    align-items: center; 
+    justify-content: center;
+}
+</style>

19319467176589c53986bd243debd2ad85b21fd7
Savyan <mrsavyan555@gmail.com>
2024-04-14 12:15:57 +0300

    add letters and animation

--- kon/src/KonApp.vue
+++ kon/src/KonApp.vue
@@ -2,18 +2,42 @@
 </script>
 
 <template>
-    <div class="circle">KON</div>
+    <div class="circle">
+        <div class="kon-label">KON</div>
+    </div>
 </template>
 
 <style scoped>
 .circle {
     border: 10px dashed red;
-    height: 100px;
-    width: 100px;
+    height: 300px;
+    width: 300px;
     border-radius: 50%;
 
-    display: flex; /* or inline-flex */
+    display: flex;
     align-items: center; 
     justify-content: center;
+
+    /* transition-duration: 0.8s;
+    transition-property: transform;
+    animation:spin 3s linear reverse infinite; */
 }
+
+.kon-label {
+    position: absolute;
+
+    font-style: italic;
+    font-size: 140px;
+    font-family: 'Bebas Neue';
+    color: whitesmoke;
+    text-shadow: 0 0 10px black;
+    /* -webkit-text-stroke: 1px black; */
+    /* animation:spin 3s linear infinite; */
+}
+
+@keyframes spin { 100% { transform:rotate(360deg); } }
+
+/* .circle:hover {
+    transform: rotate(360deg);
+} */
 </style>
